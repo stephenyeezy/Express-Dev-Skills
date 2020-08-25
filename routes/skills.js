3444980a426b68fd6080d7ab64149c44203f1054
stephen yi <stephenyi@stephens-MacBook-Pro.local>
2020-08-25 17:34:08 -0400

    update

--- routes/skills.js
+++ routes/skills.js
@@ -3,19 +3,12 @@ var router = express.Router();
 var skillsCtrl = require('../controllers/skills');
 const { route } = require('.');
 
-// GET /todos
 router.get('/', skillsCtrl.index);
-// GET /todos/new
 router.get('/new', skillsCtrl.new);
-// GET /todos/<some id>
 router.get('/:id', skillsCtrl.show);
-// POST /todos
 router.post('/', skillsCtrl.create);
-// DELETE /todos/:id
 router.delete('/:id', skillsCtrl.delete);
-// GET /todos/:id/edit
 router.get('/:id/edit', skillsCtrl.edit);
-// PUT /todos/:id
 router.put('/:id', skillsCtrl.update);
 
 
